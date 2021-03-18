a1002c25ca587086689643795928bfaccab45fd9
Zinan Guo <zinan@vt.edu>
2021-03-18 15:31:03 -0400

    visualization update

--- extension/popup.js
+++ extension/popup.js
@@ -1,19 +1,85 @@
 var bg = chrome.extension.getBackgroundPage();
 
+var monitorStarted = false;
+var monitorStartTime;
+var timeUpdate
+
 update_tabCount = function () {
     document.getElementById("tab_counter").innerText = "Number of tabs opened:" + bg.count;
 }
 
-click_events = function () {
-    /* document.getElementById("stop_monitor").addEventListener("click", function() {
-        alert("monitoring stoped");
-    }); */
-    document.getElementById("start_monitor").addEventListener("click", function () {
-        alert("monitoring started");
-    });
-};
+
+
+var formatTime = function (TimeDiff) {
+    var hour = parseInt(TimeDiff / (60 * 60 * 1000));
+    var afterHour = TimeDiff - hour * 60 * 60 * 1000;
+    var min = parseInt(afterHour / (60 * 1000));
+    var afterMin = TimeDiff - hour * 60 * 60 * 1000 - min * 60 * 1000;
+    var second = parseInt(afterMin / 1000)
+    var output = hour.toString() + 'H ' + min.toString() + 'M ' + second.toString() + 'S';
+    return output;
+}
+
+var timer = function () {
+    var currTime = new Date();
+    var timeElapsed = currTime.getTime() - monitorStartTime.getTime();
+    document.getElementById("monitor_timer").innerText = formatTime(timeElapsed);
+}
 
 //content
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    for (var i=0; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                tarr.push(data[j].substring(1, data[j].length-1));
+            }
+            lines.push(tarr);
+    }
+   return lines;
+}
+
+//load data
+var power;
+var cpu;
+var gpu;
+var processPower = function(data){
+    power = data[2][2];
+    document.getElementById("curr_power").innerText = "Current power usage: " + power + "W";
+}
+var processRes = function(data){
+    cpu = data[3][1];
+    gpu = data[4][1]
+    document.getElementById("cpu_info").innerText = "CPU usage: " + cpu + "%";
+    document.getElementById("gpu_info").innerText = "GPU usage: " + gpu + "%";
+}
+chrome.runtime.getPackageDirectoryEntry(function (root) {
+    root.getFile("data/output_RES.csv", {}, function (fileEntry) {
+        fileEntry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function (e) {
+                var res_data = processData(e.target.result);
+                processRes(res_data);
+            };
+            reader.readAsText(file);
+        });
+    });
+    root.getFile("data/output_POW.csv", {}, function (fileEntry) {
+        fileEntry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function (e) {
+                var power_data = processData(e.target.result)
+                processPower(power_data);
+            };
+            reader.readAsText(file);
+        });
+    });
+});
+
+
+//chart
 google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
@@ -58,7 +124,7 @@ function drawChart() {
         },
         vAxis: {
             textStyle: { color: '#FFF', fontSize: 10 },
-            baselineColor:{
+            baselineColor: {
                 color: 'transparent'
             }
         },
@@ -77,6 +143,24 @@ chrome.runtime.onMessage.addListener(
         if (request.msg == "tab_count_update")
             update_tabCount();
     });
+
+click_events = function () {
+    document.getElementById("start_monitor").addEventListener("click", function () {
+        if (monitorStarted == false) {
+            monitorStartTime = new Date();
+            timeUpdate = setInterval(timer, 1000);
+            document.getElementById("start_monitor").innerText = "stop monitor";
+            monitorStarted = true;
+        } else {
+            document.getElementById("start_monitor").innerText = "start monitor";
+            clearInterval(timeUpdate);
+            monitorStarted = false;
+            document.getElementById("button_layer").style.visibility = "hidden";
+            document.getElementById("details").style.cssText = "filter: blur(0px);";;
+            /* .style.filter = "filter: blur(10px);"; */
+        }
+    });
+};
 
 document.addEventListener("DOMContentLoaded", function () {
     click_events();
