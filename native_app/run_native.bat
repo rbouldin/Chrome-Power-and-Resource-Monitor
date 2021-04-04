501e927cf7d2cb828d1e03690e8191bbe208aed5
Rae Bouldin <raebouldin@gmail.com>
2021-04-04 16:38:12 -0400

    Add more options

--- native_app/run_native.bat
+++ native_app/run_native.bat
@@ -1,14 +1,25 @@
 ::
-:: Run Options: "monitor.jar [-records int] [-native on] [-native off] [-server on] [-server off] [-clean]"
+:: Run Options: java -jar "monitor.jar" [OPTIONS]"
 ::
-:: -records int       An integer number of records you'd like to record (the number of times the 
-::                    monitor will loop). This is 60 by default. Each loop should take ~2-3 seconds.
+:: OPTIONS:
 ::
-:: -native [on/off]   Turn native messaging on or off. It is ON by default.
+::   -records int            An integer number of records you'd like to record (the number of 
+::                           times the monitor will loop). Each loop should take ~2-3 seconds. 
+::                           This is 60 by default.
 ::
-:: -server [on/off]   Turn server messaging on or off. It is OFF by default.
+::   -native [on|off]        Turn native messaging output & input on or off. It is ON by default.
+::   -nativeOutput [on|off]  Turn native messaging output on or off. It is ON by default.
+::   -nativeInput [on|off]   Turn native messaging input on or off. It is ON by default.
 ::
-:: -clean             Deletes any output files created by previous runs then exits.
+::   -server [on|off]        Turn server messaging on or off. It is OFF by default.
+::
+::   -logNative              Ouput the messages sent back and forth in native messaging to a 
+::                           NativeMessageLog txt file.
+::
+::   -logServer              Ouput the messages sent back and forth in server messaging to a 
+::                           ServerMessageLog txt file.
+::
+::   -clean                  Deletes any output files created by previous runs then exits.
 ::
 @echo off
-java -jar "monitor.jar" -records 60 -native on -server off %*
+java -jar "monitor.jar" -records 60 -nativeOutput on -nativeInput off %*
