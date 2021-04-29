88053e23c2d592a3d3e4f9c0b0700f776188a749
Rae Bouldin <rbouldin@vt.edu>
2021-04-29 10:24:07 -0400

    Delete native_app/monitor_src/powershell_scripts directory

--- native_app/monitor_src/powershell_scripts/sendPowerDataToCSV.ps1
+++
@@ -1,8 +0,0 @@
-Get-Wmiobject Sensor -namespace root\OpenHardwareMonitor |
-    Where-Object { $_.SensorType -eq "Power" } |
-    Sort-Object -Property InstanceId |
-    Select Name,
-    @{Name="SensorType";Expression={[String]$_.SensorType}},
-    @{Name="Value (W)";Expression={[String]$_.Value}},
-    @{Name="ProcessID";Expression={[String]$_.ProcessId}} |
-Export-CSV -Path monitor_POW_output.csv
