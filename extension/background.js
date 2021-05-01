aed73c3d4326a4c6de1724a89dafbc1012ad4917
Zinan Guo <zinan@vt.edu>
2021-05-01 14:09:33 -0400

    keep monitor running after close the popup

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,26 @@
+//global variables (settings)
+var ACTIVE_MONITOR_INTERVAL = 1;
+var BG_MONITOR_INTERVAL = 10;
+var CARBON_INTERVAL = 20;
+
+var newUser = true;
+//counters
 var count = 0;
 var totalCO2 = 0;
 var avgPower = 5;
+var avgPowerLoggedTime = 0;
+
 
 //native data query
 //=======================================================
 
 //unique user id --------------------
 var getRandomToken = function () {
-    var randomPool = new Uint8Array(16);
+    var randomPool = new Uint8Array(4);
     crypto.getRandomValues(randomPool);
     var hex = '';
     for (var i = 0; i < randomPool.length; ++i) {
-        hex += randomPool[i].toString(8);
+        hex += randomPool[i].toString(16);
     }
     return hex;
 }
@@ -33,8 +42,45 @@ var userIdFunc = function (varFunc) {
 //native message --------------------
 var nativePort;
 var popupPort;
+var inSession = false;
 var popupConnected = false;
 var nativeConnected = false;
+
+var sessionData = [[0, 0, 0, 0]];
+var sessionIndex = 0;
+var sessionPower = 0;
+var currPower = 0;
+var cpuPackage = 0;
+var lastQuery = new Date();
+var monitorStartTime = new Date();
+
+var calculate_power = function (cpuPercentage) {
+    return (cpuPercentage * cpuPackage) / (cpuPercentage * 0.412 + 0.246);
+}
+
+var updateAvgPower = function () {
+    var currTime = new Date();
+    var sessionTime = (currTime.getTime() - monitorStartTime.getTime()) / (3600 * 1000);
+    var sessionAvgPower = sessionPower / sessionTime;
+
+    if (sessionAvgPower != 0){
+        avgPower = (avgPowerLoggedTime * avgPower + sessionAvgPower * sessionTime) / (avgPowerLoggedTime + sessionTime);
+        avgPowerLoggedTime += sessionTime;
+        chrome.storage.sync.set({ avgPower: avgPower });
+        chrome.storage.sync.set({ avgPowerLoggedTime: avgPowerLoggedTime });
+    }
+}
+
+
+var resetSessionData = function() {
+    sessionData = [[0, 0, 0, 0]];
+    sessionIndex = 0;
+    sessionPower = 0;
+    currPower = 0;
+    cpuPackage = 0;
+    lastQuery = new Date();
+    monitorStartTime = new Date();
+}
 
 var sendPost = function (userId) {
     nativePort.postMessage({ "message": "POST user", "user_id": "" + userId, "suggestions": [], "tabs": "" + count });
@@ -48,23 +94,36 @@ var listenNative = function (msg) {
 
     var maxCpu = parseFloat(msg.max_cpu_power);
     if (!Number.isNaN(maxCpu)){
-        popupPort.postMessage({type: "maxCpuPower", power: maxCpu});
+        cpuPackage = maxCpu;
     }
     else {
         var cpuData = parseFloat(msg.cpu_usage);
         var gpuData = parseFloat(msg.gpu_usage);
         var memData = parseFloat(msg.mem_usage);
-        popupPort.postMessage({type: "newData", cpu: cpuData, gpu: gpuData, mem: memData});
+        sessionIndex++;
+        sessionData.push([sessionIndex, cpuData, gpuData, memData]);
+
+        currPower = calculate_power(cpuData / 100)
+        var currTime = new Date();
+        var timeElapsed = (currTime.getTime() - lastQuery.getTime()) / (3600 * 1000);
+        sessionPower += currPower * timeElapsed;
+        lastQuery = new Date();
+
+        if(popupConnected){
+            popupPort.postMessage({type: "newData", cpu: cpuData, gpu: gpuData, mem: memData, curr: currPower, session: sessionPower});
+        }
     }
 }
 
 var connectNative = function () {
+    newUser = false;
+    inSession = true;
     nativeConnected = true;
+    resetSessionData();
+
     nativePort = chrome.runtime.connectNative("com.chrome.monitor");
     nativePort.onMessage.addListener(listenNative);
-    nativePort.onDisconnect.addListener(function() {
-        console.log("Disconnected");
-    });
+    nativePort.onDisconnect.addListener(function() {console.log("Native Disconnected");});
     nativePort.postMessage({ "message": "connected" });
     userIdFunc(sendPost);
 }
@@ -75,10 +134,34 @@ var updateData = function () {
     }
 }
 
+var bgTimer;
+var fgTimer;
+
+var pauseMonitor = function () {
+    clearInterval(fgTimer);
+}
+
+var resumeMonitor = function () {
+    fgTimer = setInterval(updateData, ACTIVE_MONITOR_INTERVAL * 1000);
+}
+
 var stopMonitor = function () {
+    inSession = false;
     nativeConnected = false;
-    nativePort.postMessage({ "message": "STOP monitoring" });
+    clearInterval(fgTimer);
+    clearInterval(bgTimer);
     nativePort.postMessage({ "message": "GET suggestions" });
+    nativePort.postMessage({ "message": "EXIT NATIVE" });
+}
+
+var moveMonitorToBg = function () {
+    clearInterval(fgTimer);
+    bgTimer = setInterval(updateData, BG_MONITOR_INTERVAL * 1000);
+}
+
+var moveMonitorToFg = function () {
+    clearInterval(bgTimer);
+    fgTimer = setInterval(updateData, ACTIVE_MONITOR_INTERVAL * 1000);
 }
 
 chrome.runtime.onConnect.addListener(function(dataPort) {
@@ -88,22 +171,24 @@ chrome.runtime.onConnect.addListener(function(dataPort) {
         popupPort.onMessage.addListener(function(msg) {
             if (msg.type == "startMonitor"){
                 connectNative();
+                moveMonitorToFg();
             } else if (msg.type == "stopMonitor"){            
                 stopMonitor();
-            } else if (msg.type == "dataUpdate"){
-                updateData();
-            } else if (msg.type == "avgPower"){
-                avgPower = msg.power;
-                console.log("avgPowerUpdate", avgPower);
-                chrome.storage.sync.set({ avgPower: avgPower });
+            } else if (msg.type == "pause"){
+                pauseMonitor();
+            } else if (msg.type == "resume"){
+                resumeMonitor();
             }
         });
         popupPort.onDisconnect.addListener(function() {
             popupConnected = false;
-            if (nativeConnected){
-                stopMonitor();
+            if (inSession){
+                moveMonitorToBg();
             }
         });
+        if (inSession){
+            moveMonitorToFg();
+        }
     }
 });
 
@@ -140,6 +225,15 @@ chrome.storage.sync.get('avgPower', function (items) {
     }
 });
 
+chrome.storage.sync.get('avgPowerLoggedTime', function (items) {
+    var loggedTime = items.avgPowerLoggedTime;
+    if (loggedTime) {
+        avgPowerLoggedTime = loggedTime;
+    } else {
+        chrome.storage.sync.set({ avgPowerLoggedTime: 0 });
+    }
+});
+
 chrome.storage.sync.get('totalCO2', function (items) {
     var lastCO2 = items.totalCO2;
     if (lastCO2) {
@@ -151,15 +245,14 @@ chrome.storage.sync.get('totalCO2', function (items) {
 
 //timer to count the CO2 ---------------------
 var carbonTimer;
-var timeInterval = 5;
 
 var carbonCounter = function(){
-    totalCO2 += (timeInterval / 3600) * avgPower * 0.4173;
+    totalCO2 += (CARBON_INTERVAL / 3600) * avgPower * 0.4173;
     chrome.storage.sync.set({ totalCO2: totalCO2 });
     if (popupConnected){
         popupPort.postMessage({type: "carbonUpdate"});
     }
 }
 
-carbonTimer = setInterval(carbonCounter, timeInterval * 1000);
+carbonTimer = setInterval(carbonCounter, CARBON_INTERVAL * 1000);
 
--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,23 @@
-//determine whether need to blur
-var newUser = true;
+//visual parts
+var hideButtonLayer = function () {
+    document.getElementById("button_layer").style.visibility = "hidden";
+    document.getElementById("details").style.cssText = "filter: blur(0px);";
+}
+
+var showButtonLayer = function () {
+    document.getElementById("details").style.cssText = "filter: blur(3px) brightness(70%);";
+    document.getElementById("button_layer").style.visibility = "visible";
+}
+
+var changeStoppedButtons = function () {
+    document.getElementById("pause_start_btn").innerText = "Start timer";
+    document.getElementById("stop_start_btn").innerText = "Start";
+}
+
+var changeStartedButtons = function () {
+    document.getElementById("pause_start_btn").innerText = "Pause";
+    document.getElementById("stop_start_btn").innerText = "Stop";
+}
 
 //header info module
 //============================
@@ -42,73 +60,70 @@ var formatTime = function (TimeDiff) {
 
 //Timer -------------------
 var timer;
-var dataTimer = function () {
+var timeTimer = function () {
     var currTime = new Date();
     var timeElapsed = currTime.getTime() - monitorStartTime.getTime();
     document.getElementById("monitor_timer").innerText = formatTime(timeElapsed);
-    dataPort.postMessage({ type: "dataUpdate" });
 }
 
 //data chart
 //===========================
 var data;
 var chart;
-var option;
 var chartElement;
-
-google.charts.load('current', { 'packages': ['corechart'] }).then(function () {
-    data = new google.visualization.DataTable();
-    data.addColumn('number', 'Time(Second)');
-    data.addColumn('number', 'CPU usage');
-    data.addColumn('number', 'GPU usage');
-    data.addColumn('number', 'MEM usage');
-    data.addRow([0, 0, 0, 0]);
-
-    options = {
-        width: 420,
-        height: 170,
-        chartArea: { width: '100%', height: '100%' },
-        theme: 'maximized',
-        backgroundColor: 'transparent',
-        hAxis: {
-            viewWindow: { min: 0, max: 10 },
-            textStyle:
-            {
-                color: '#FFF',
-                fontSize: 10
-            },
-            baselineColor: {
-                color: 'transparent'
-            },
-            gridlines:
-            {
-                color: 'transparent',
-                count: 10
-            }
-        },
-        vAxis: {
-            viewWindowMode: 'maximized',
-            viewWindow: { min: 0, max: 100 },
-            textStyle: { color: '#FFF', fontSize: 10 },
-            baselineColor: {
-                color: 'transparent'
-            }
-        },
-        legend: {
-            textStyle: { color: '#FFF', fontSize: 10 }
-        },
-    };
-
-    var drawChart = function () {
-        chartElement = document.getElementById('power_chart');
-        chart = new google.visualization.LineChart(chartElement);
-        chart.draw(data, options);
-    }
-    google.charts.setOnLoadCallback(drawChart);
-})
-
 var chartIndex = 0;
-var maxDataPoints = 10
+var maxDataPoints = 10;
+
+var options = {
+    width: 420,
+    height: 170,
+    chartArea: { width: '100%', height: '100%' },
+    theme: 'maximized',
+    backgroundColor: 'transparent',
+    hAxis: {
+        viewWindow: { min: 0, max: maxDataPoints },
+        textStyle: {
+            color: '#FFF',
+            fontSize: 10
+        },
+        baselineColor: {
+            color: 'transparent'
+        },
+        gridlines: {
+            color: 'transparent',
+            count: 10
+        }
+    },
+    vAxis: {
+        viewWindowMode: 'maximized',
+        viewWindow: { min: 0, max: 100 },
+        textStyle: { color: '#FFF', fontSize: 10 },
+        baselineColor: { color: 'transparent' }
+    },
+    legend: {
+        textStyle: { color: '#FFF', fontSize: 10 }
+    },
+    explorer: { axis: 'horizontal' }
+};
+
+var drawEmpty = function () {
+    google.charts.load('current', { 'packages': ['corechart'] }).then(function () {
+        data = new google.visualization.DataTable();
+        data.addColumn('number', 'Time(Second)');
+        data.addColumn('number', 'CPU usage');
+        data.addColumn('number', 'GPU usage');
+        data.addColumn('number', 'MEM usage');
+        data.addRow([0, 0, 0, 0]);
+    
+        var drawChart = function () {
+            chartElement = document.getElementById('power_chart');
+            chart = new google.visualization.LineChart(chartElement);
+            chart.draw(data, options);
+        }
+        google.charts.setOnLoadCallback(drawChart);
+    });
+}
+
 var updateData = function (cpuNum, gpuNum, memNum) {
     chartIndex++;
     data.addRow([chartIndex, cpuNum, gpuNum, memNum]);
@@ -116,6 +131,12 @@ var updateData = function (cpuNum, gpuNum, memNum) {
         options.hAxis.viewWindow.max = chartIndex;
         options.hAxis.viewWindow.min = chartIndex - maxDataPoints;
     }
+    chart.draw(data, options);
+}
+
+var finishChart = function () {
+    options.hAxis.viewWindow.max = chartIndex;
+    options.hAxis.viewWindow.min = 0;
     chart.draw(data, options);
 }
 
@@ -127,89 +148,94 @@ var clearChart = function () {
     data.addColumn('number', 'MEM usage');
     data.addRow([0, 0, 0, 0]);
     chartIndex = 0;
+
+    options.hAxis.viewWindow.min = 0;
+    options.hAxis.viewWindow.max = maxDataPoints;
     chart.draw(data, options);
 
+    document.getElementById("curr_power").innerText = "Current power usage: 0.00 W";
+    document.getElementById("total_power").innerText = "Total power usage: 0.00 Wh";
     document.getElementById("cpu_info").innerText = "CPU: 00.00%";
     document.getElementById("gpu_info").innerText = "GPU: 00.00%";
     document.getElementById("mem_info").innerText = "MEM: 00.00%";
 }
 
+var loadFromBg = function () {
+    google.charts.load('current', { 'packages': ['corechart'] }).then(function () {
+        data = new google.visualization.DataTable();
+        data.addColumn('number', 'Time(Second)');
+        data.addColumn('number', 'CPU usage');
+        data.addColumn('number', 'GPU usage');
+        data.addColumn('number', 'MEM usage');
+        data.addRows(bg.sessionData);
+        chartIndex = bg.sessionIndex;
+        if (chartIndex > maxDataPoints) {
+            options.hAxis.viewWindow.max = chartIndex;
+            options.hAxis.viewWindow.min = chartIndex - maxDataPoints;
+        }
+        chartElement = document.getElementById('power_chart');
+        chart = new google.visualization.LineChart(chartElement);
+        chart.draw(data, options);
+    });
+    document.getElementById("curr_power").innerText = "Current power usage: 0.00 W";
+    document.getElementById("total_power").innerText = "Total power usage: " + bg.sessionPower.toFixed(2) + " Wh"
+}
+
 //native message
 //============================
 var monitorStarted = false;
-var lastQuery = new Date();
-var cpuPackage;
-var totalPower;
-var calculate_power = function (cpuPercentage) {
-    return (cpuPercentage * cpuPackage) / (cpuPercentage * 0.412 + 0.246);
-}
 
-var receiveData = function(cpu, gpu, mem){
-    if (!monitorStarted){
+var receiveData = function (cpu, gpu, mem, currPower, sessionPower) {
+    if (!monitorStarted) {
         return;
     }
 
     document.getElementById("cpu_info").innerText = "CPU: " + cpu.toFixed(2) + "%";
     document.getElementById("mem_info").innerText = "MEM: " + mem.toFixed(2) + "%";
-    if (gpu != null){
+    if (gpu != null) {
         //in case open hardware monitor is not opened
         document.getElementById("gpu_info").innerText = "GPU: " + gpu.toFixed(2) + "%";
     }
 
-    var currTime = new Date();
-    //time elapse in hour
-    var timeElapsed = (currTime.getTime() - lastQuery.getTime()) / (3600 * 1000);
-    var currPower = calculate_power(cpu / 100);
-    totalPower += currPower * timeElapsed;
-
     var powerDisplay = document.getElementById("curr_power").childNodes[0];
     powerDisplay.nodeValue = "Current power usage: " + currPower.toFixed(2) + " W";
-    document.getElementById("total_power").innerText = "Total power usage: " + totalPower.toFixed(2) + " Wh"
+    document.getElementById("total_power").innerText = "Total power usage: " + sessionPower.toFixed(2) + " Wh"
     updateData(cpu, gpu, mem);
-    lastQuery = new Date();
 }
 
-var newSession = function(){
+var newSession = function () {
     monitorStarted = true;
+    clearChart();
+    changeStartedButtons();
+    dataPort.postMessage({ type: "startMonitor" });
 
-    totalPower = 0;
-    lastQuery = new Date();
     monitorStartTime = new Date();
-    
-    dataPort.postMessage({type: "startMonitor"});
-    timer = setInterval(dataTimer, 1000);
-    document.getElementById("button_layer").style.visibility = "hidden";
-    document.getElementById("details").style.cssText = "filter: blur(0px);";
+    timer = setInterval(timeTimer, 1000);
+
+    if (bg.newUser) {
+        hideButtonLayer();
+    }
 }
 
-var endSession = function(){   
-    clearInterval(timer);
+var endSession = function () {
     monitorStarted = false;
-    
-    dataPort.postMessage({type: "stopMonitor"});
-    var currTime = new Date();
-    var timeElapsed = (currTime.getTime() - monitorStartTime.getTime()) / (3600 * 1000);
-    var avgPower = totalPower / timeElapsed;
-    dataPort.postMessage({type: "avgPower", power: avgPower});
-    
-    clearChart();
-    document.getElementById("details").style.cssText = "filter: blur(3px) brightness(70%);";
-    document.getElementById("button_layer").style.visibility = "visible";
+    changeStoppedButtons();
+    dataPort.postMessage({ type: "stopMonitor" });
+
+    clearInterval(timer);
+    finishChart();
 }
 
 
 //binding events
 //=============================
-dataPort.onMessage.addListener(function(msg) {
-    if (msg.type == "tabCountUpdate"){
+dataPort.onMessage.addListener(function (msg) {
+    if (msg.type == "tabCountUpdate") {
         updateTabCount();
-    }else if (msg.type == "carbonUpdate"){
+    } else if (msg.type == "carbonUpdate") {
         updateCarbonFootprint();
-    }
-    else if (msg.type == "maxCpuPower"){
-        cpuPackage = msg.power;
-    } else if (msg.type == "newData"){
-        receiveData(msg.cpu, msg.gpu, msg.mem);
+    } else if (msg.type == "newData") {
+        receiveData(msg.cpu, msg.gpu, msg.mem, msg.curr, msg.session);
     }
 })
 
@@ -218,22 +244,49 @@ let click_events = function () {
     document.getElementById("start_monitor").addEventListener("click", function () {
         newSession();
     });
-    document.getElementById("stop_monitor").addEventListener("click", function () {
-        endSession();
-    });
-    document.getElementById("pause_monitor").addEventListener("click", function () {
-        if (paused){
-            document.getElementById("pause_monitor").innerText = "Pause";
-            timer = setInterval(dataTimer, 1000);
+    document.getElementById("stop_start_btn").addEventListener("click", function () {
+        if (monitorStarted) {
+            endSession();
         } else {
-            document.getElementById("pause_monitor").innerText = "Resume";
-            clearInterval(timer);
+            newSession();
         }
-        paused = !paused;
+    });
+    document.getElementById("pause_start_btn").addEventListener("click", function () {
+        if (monitorStarted) {
+            if (paused) {
+                dataPort.postMessage({ type: "resume" });
+                timer = setInterval(timeTimer, 1000);
+                document.getElementById("pause_start_btn").innerText = "Pause";
+            } else {
+                dataPort.postMessage({ type: "pause" });
+                clearInterval(timer);
+                document.getElementById("pause_start_btn").innerText = "Resume";
+            }
+            paused = !paused;
+        } else {
+            //timer later
+        }
     });
 };
 
+var initBasedOnStatus = function () {
+    //if the user is opening the popup the first time since chrome started
+    if (!bg.newUser) {
+        hideButtonLayer();
+    }
+
+    //if the background is running one session
+    monitorStarted = bg.inSession;
+    if (monitorStarted) {
+        loadFromBg();
+    } else {
+        drawEmpty();
+        changeStoppedButtons();
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    initBasedOnStatus();
     click_events();
     updateTabCount();
     updateCarbonFootprint();
